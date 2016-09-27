dd18a2ceda342fde45cc1518ac625f83d65b4b39
unostore <1island@gmail.com>
2016-09-27 05:24:24 -0700

    two

--- config/config.js
+++ config/config.js
@@ -5,8 +5,8 @@ module.exports = {
     MONGODB_URI: '',
     MONGOLAB_URI: 'mongodb://saad:saad@ds045011.mlab.com:45011/meanjs',
     SESSION_SECRET: '%N3duAKUY59NC6w##u&debZFB#W95nUM',
-    FACEBOOK_ID: '827164310754437',
-    FACEBOOK_SECRET: 'b60e68b64db49602eafbb9a75c85ecc4',
+    FACEBOOK_ID: '1982922898600712',
+    FACEBOOK_SECRET: '929d689033e9bb29be036eca551f2634',
     LINKEDIN_ID: '81w3amr1h1zb1l',
     LINKEDIN_SECRET: 'ndbiz7sLaPGjxpQw',
     LINKEDIN_CALLBACK_URL: 'http://138.68.60.219:3000//auth/linkedin/callback'
