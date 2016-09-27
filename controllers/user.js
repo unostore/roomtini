fe2a0e6fb66e02cf7c181b6750381970168c3f60
unostore <1island@gmail.com>
2016-09-26 22:27:24 -0700

    two

--- controllers/user.js
+++ controllers/user.js
@@ -10,7 +10,7 @@ const User = require('../models/User');
  */
 exports.getLogin = (req, res) => {
   if (req.user) {
-    return res.redirect('/');
+    return res.redirect('/account');
   }
   res.render('account/login', {
     title: 'Login'
