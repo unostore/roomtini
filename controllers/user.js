afb994926374c07b4d97959a44846d7c84fcdbe7
unostore <1island@gmail.com>
2016-09-30 17:25:42 -0700

    first sendgrid env

--- controllers/user.js
+++ controllers/user.js
@@ -354,8 +354,8 @@ exports.postForgot = (req, res, next) => {
       });
       const mailOptions = {
         to: user.email,
-        from: 'hackathon@starter.com',
-        subject: 'Reset your password on Hackathon Starter',
+        from: 'reset@Roomtini.com',
+        subject: 'Reset your password on Roomtini',
         text: `You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\n
           Please click on the following link, or paste this into your browser to complete the process:\n\n
           http://${req.headers.host}/reset/${token}\n\n
