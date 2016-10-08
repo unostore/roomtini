8a36d9a29348ea92959cedbab10b1e323383e25f
unostore <1island@gmail.com>
2016-10-08 01:47:39 -0700

    analytics

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ var access_logfile = fs.createWriteStream('./access.log', {flags: 'a'});
 app.configure(function(){
     app.use(express.logger({stream: access_logfile }));
     app.set('views', __dirname + '/views');
-    ...
+
 });
 
 //for favicon
