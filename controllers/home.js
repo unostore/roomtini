4465fc1cdfd7e759cbc4b8f99d2c5bc1e317f585
mirzasaad <saaad.mirxa@gmail.com>
2016-10-09 06:36:45 +0500

    ...

--- controllers/home.js
+++ controllers/home.js
@@ -2,28 +2,38 @@
  * GET /
  * Home page.
  */
+
 exports.index = (req, res) => {
   res.render('home', {
     title: 'Home'
   });
 };
+
 exports.indexOne = (req, res) => {
   res.render('index', {
     title: 'Landing Page'
   });
 };
+
 exports.settings = (req, res) => {
   res.render('settings', {
     title: 'Settings'
   });
 };
+
 exports.billings = (req, res) => {
   res.render('billings', {
     title: 'Billings'
   });
 };
+
 exports.events = (req, res) => {
   res.render('events', {
     title: 'Events'
   });
+};
+
+exports.typeform = (req, res, done) => {
+  var fs = require("fs");
+  fs.appendFile('../test.js', JSON.stringify(req.body));
 };
