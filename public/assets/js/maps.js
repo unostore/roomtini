304da4056b1317b45005122b4b6dd23627df7515
mirzasaad <saaad.mirxa@gmail.com>
2016-12-22 12:29:01 +0500

    ...

--- public/assets/js/maps.js
+++ public/assets/js/maps.js
@@ -28,9 +28,31 @@ function createHomepageGoogleMap(_latitude,_longitude,json){
         gMap();
     });
     function gMap(){
+
+        var isMobile = {
+            Android: function() {
+                return navigator.userAgent.match(/Android/i);
+            },
+            BlackBerry: function() {
+                return navigator.userAgent.match(/BlackBerry/i);
+            },
+            iOS: function() {
+                return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+            },
+            Opera: function() {
+                return navigator.userAgent.match(/Opera Mini/i);
+            },
+            Windows: function() {
+                return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+            },
+            any: function() {
+                return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+            }
+        };
+
         var mapCenter = new google.maps.LatLng(_latitude,_longitude);
         var mapOptions = {
-            zoom: 14,
+            zoom: isMobile.any() ? 12 : 14,
             center: mapCenter,
             disableDefaultUI: false,
             scrollwheel: false,
